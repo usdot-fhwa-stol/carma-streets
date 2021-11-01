dc9a7000819f367fe91007035d957a994874631f
dan-du-car <dan.du@leidos.com>
2021-11-01 12:30:14 -0700

    init

--- message_services/src/message_services.cpp
+++ message_services/src/message_services.cpp
@@ -1,6 +1,18 @@
 #include <thread>
+#include "vehicle_status_intent_service.h"
+
+void vehicle_status_intent_service_call()
+{
+    message_services::services::vehicle_status_intent_service s_v;
+    if (s_v.initialize())
+    {
+        s_v.start();
+    }
+}
 
 int main(int argc, const char **argv)
 {
+    std::thread vehicle_status_intent_service_t(vehicle_status_intent_service_call);
+    vehicle_status_intent_service_t.join();
     return 0;
 }
