dac964866d20a639f6325a546de9a26be1755361
dan-du-car <dan.du@leidos.com>
2021-11-04 09:28:48 -0700

    update unit test

--- message_services/test/test_message_lanelet2_translation.cpp
+++ message_services/test/test_message_lanelet2_translation.cpp
@@ -82,6 +82,34 @@ TEST(test_message_lanelet2_translation, get_cur_lanelets_by_point)
     ASSERT_EQ(1, clt.get_cur_lanelets_by_point(basic_point3d).size());
     ASSERT_EQ(12459, clt.get_cur_lanelets_by_point(basic_point3d).front().id());
 }
+
+TEST(test_message_lanelet2_translation, distance2_cur_lanelet_end_point)
+{
+    message_services::message_translations::message_lanelet2_translation clt("../vector_map.osm");
+
+    //Position within the link lanelet with turn direction
+    lanelet::Point3d point3d = {lanelet::utils::getId(), {-83.21549512455204, 329.05751672287005, 72}};
+    ASSERT_NEAR(15.033, clt.distance2_cur_lanelet_end(point3d, "right"), 0.01);
+
+    //Position within the entry lanelet
+    point3d = {lanelet::utils::getId(), {-96.40002062426407,304.71216057912693, 72}};
+    ASSERT_NEAR(17.4779, clt.distance2_cur_lanelet_end(point3d, ""), 0.01);
+
+    point3d = {lanelet::utils::getId(), {-91.5717516026476, 313.7486930462219, 72}};
+    ASSERT_NEAR(7.12777, clt.distance2_cur_lanelet_end(point3d, ""), 0.01);
+
+    point3d = {lanelet::utils::getId(), {-91.04298660840287,314.4369775722761, 72}};
+    ASSERT_NEAR(6.08981, clt.distance2_cur_lanelet_end(point3d, ""), 0.01);
+
+    //Position within the departure lanelet
+    point3d = {lanelet::utils::getId(), {-66.33865452644577,327.63636981163864, 72}};
+    ASSERT_NEAR(137.707, clt.distance2_cur_lanelet_end(point3d, ""), 0.01);
+
+    point3d = {lanelet::utils::getId(), {-64.42300140950572,327.1034837742677, 72}};
+    ASSERT_NEAR(135.699, clt.distance2_cur_lanelet_end(point3d, ""), 0.01);
+}
+
+
 TEST(test_message_lanelet2_translation, distance2_cur_lanelet_end)
 {
     message_services::message_translations::message_lanelet2_translation clt("../vector_map.osm");
@@ -102,21 +130,38 @@ TEST(test_message_lanelet2_translation, distance2_cur_lanelet_end)
 TEST(test_message_lanelet2_translation, get_lanelet_types_ids_by_route)
 {
     message_services::message_translations::message_lanelet2_translation clt("../vector_map.osm");
-
-    //From lanelet id = 19252 to 22414 to lanelet id = 12459
+    
+    //From lanelet id = 19252  (entry lanelet) to lanelet id = 19252 (entry lanelet)
     std::int64_t start_lanelet_id = 19252;
-    std::int64_t dest_lanelet_id = 12459;
-    ASSERT_EQ(3, clt.get_lanelet_types_ids_by_route(start_lanelet_id, dest_lanelet_id).size());
+    std::int64_t dest_lanelet_id = 19252;
+    ASSERT_EQ(1, clt.get_lanelet_types_ids_by_route(start_lanelet_id, dest_lanelet_id).size());
+    ASSERT_EQ(message_services::models::entry, clt.get_lanelet_types_ids_by_route(start_lanelet_id, dest_lanelet_id).at(start_lanelet_id));
 
-    //From lanelet id = 22414 to lanelet id = 12459
+    //From lanelet id = 19252 (entry lanelet) to 22414 (link lanelet) to lanelet id = 12459 (departure lanelet)
+    start_lanelet_id = 19252;
+    dest_lanelet_id = 12459;
+    ASSERT_EQ(3, clt.get_lanelet_types_ids_by_route(start_lanelet_id, dest_lanelet_id).size());
+    ASSERT_EQ(message_services::models::entry, clt.get_lanelet_types_ids_by_route(start_lanelet_id, dest_lanelet_id).at(start_lanelet_id));
+    ASSERT_EQ(message_services::models::departure, clt.get_lanelet_types_ids_by_route(start_lanelet_id, dest_lanelet_id).at(dest_lanelet_id));
+
+    //From lanelet id = 22414 (link lanelet) to lanelet id = 22414 (link lanelet)
+    start_lanelet_id = 22414;
+    dest_lanelet_id = 22414;
+    ASSERT_EQ(1, clt.get_lanelet_types_ids_by_route(start_lanelet_id, dest_lanelet_id).size());
+    ASSERT_EQ(message_services::models::link, clt.get_lanelet_types_ids_by_route(start_lanelet_id, dest_lanelet_id).at(dest_lanelet_id));
+
+    //From lanelet id = 22414 (link lanelet) to lanelet id = 12459 (departure lanelet)
     start_lanelet_id = 22414;
     dest_lanelet_id = 12459;
     ASSERT_EQ(2, clt.get_lanelet_types_ids_by_route(start_lanelet_id, dest_lanelet_id).size());
+    ASSERT_EQ(message_services::models::link, clt.get_lanelet_types_ids_by_route(start_lanelet_id, dest_lanelet_id).at(start_lanelet_id));
+    ASSERT_EQ(message_services::models::departure, clt.get_lanelet_types_ids_by_route(start_lanelet_id, dest_lanelet_id).at(dest_lanelet_id));
 
-    //From lanelet id = 12459 to lanelet id = 12459
+    //From lanelet id = 12459 (departure lanelet) to lanelet id = 12459 (departure lanelet)
     start_lanelet_id = 12459;
     dest_lanelet_id = 12459;
     ASSERT_EQ(1, clt.get_lanelet_types_ids_by_route(start_lanelet_id, dest_lanelet_id).size());
+    ASSERT_EQ(message_services::models::unknown, clt.get_lanelet_types_ids_by_route(start_lanelet_id, dest_lanelet_id).at(dest_lanelet_id));
 }
 
 TEST(test_message_lanelet2_translation, get_route_lanelet_ids_by_vehicle_trajectory)
