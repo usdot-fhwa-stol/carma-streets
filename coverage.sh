09d7db78dbc2675795d890cad90a6c096e946df4
SaikrishnaBairamoni <saikrishna.bairamoni@leidos.com>
2021-11-09 13:30:24 -0500

    Updated changes from old branch

---
+++ coverage.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+#  Copyright (C) 2018-2020 LEIDOS.
+# 
+#  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+#  use this file except in compliance with the License. You may obtain a copy of
+#  the License at
+# 
+#  http://www.apache.org/licenses/LICENSE-2.0
+# 
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#  License for the specific language governing permissions and limitations under
+#  the License.
+
+# script to run tests, generate test-coverage, and store coverage reports in a place
+# easily accessible to sonar. Test names should follow convention run<pluginName>Tests
+
+for d in kafka_clients/*
+do
+    echo ""
+    echo $d
+    if [[ -d $d ]]; then
+        if ls $d | grep run[a-zA-Z]*Tests ; then
+            TESTS="./`ls $d | grep run[a-zA-Z]*Tests`"
+            echo "$TESTS built"
+            cd $d
+            $TESTS
+            mkdir coverage
+	        gcovr -k -r .
+            mv *.gcov coverage
+            cd ../..
+        else
+            echo "no tests built"
+        fi
+    fi
+done
+for d in scheduling_service/*
+do
+    echo ""
+    echo $d
+    if [[ -d $d ]]; then
+        if ls $d | grep [a-zA-Z]*_test ; then
+            TESTS="./`ls $d | grep [a-zA-Z]*_test`"
+            echo "$TESTS built"
+            cd $d
+            $TESTS
+            mkdir coverage
+            gcovr -k -r .
+            mv *.gcov coverage
+            cd ../..
+        else
+            echo "no tests built"
+        fi
+    fi
+done
