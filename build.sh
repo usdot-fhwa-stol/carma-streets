36108990b25a3bd4213a67338ffe53fd2cd94116
SaikrishnaBairamoni <saikrishna.bairamoni@leidos.com>
2021-11-16 13:13:09 -0500

    updated build.sh and sonar properties

--- build.sh
+++ build.sh
@@ -26,3 +26,11 @@ make install
 cd /home/carma-streets/scheduling_service
 cmake -DCMAKE_CXX_FLAGS="${COVERAGE_FLAGS}" -DCMAKE_C_FLAGS="${COVERAGE_FLAGS}" -DCMAKE_BUILD_TYPE="Debug" .
 make
+
+cd /home/carma-streets/message_services
+cmake -DCMAKE_CXX_FLAGS="${COVERAGE_FLAGS}" -DCMAKE_C_FLAGS="${COVERAGE_FLAGS}" -DCMAKE_BUILD_TYPE="Debug" .
+make
+
+cd /home/carma-streets/intersection_model
+cmake -DCMAKE_CXX_FLAGS="${COVERAGE_FLAGS}" -DCMAKE_C_FLAGS="${COVERAGE_FLAGS}" -DCMAKE_BUILD_TYPE="Debug" .
+make
