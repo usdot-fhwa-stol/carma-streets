d7dc70fe8960606cccf2ec9917fc7ceac976985f
dan-du-car <dan.du@leidos.com>
2021-11-16 18:26:29 -0800

    init

---
+++ examples/qt_rest_clients_api/intersection_client/build/CMakeFiles/intersection_client.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/intersection_client_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/intersection_client_autogen.dir/ParseCache.txt"
+  "intersection_client_autogen"
+  "CMakeFiles/intersection_client.dir/intersection_client.cpp.o"
+  "CMakeFiles/intersection_client.dir/intersection_client.cpp.o.d"
+  "CMakeFiles/intersection_client.dir/intersection_client_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/intersection_client.dir/intersection_client_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/intersection_client.dir/main.cpp.o"
+  "CMakeFiles/intersection_client.dir/main.cpp.o.d"
+  "intersection_client"
+  "intersection_client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/intersection_client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ examples/qt_rest_clients_api/intersection_client/build/CMakeFiles/intersection_client.dir/intersection_client_autogen/mocs_compilation.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/intersection_client.dir/intersection_client_autogen/mocs_compilation.cpp.o: \
+ /home/ode/carma_streets_ws/carma-streets/examples/qt_rest_clients_api/intersection_client/build/intersection_client_autogen/mocs_compilation.cpp \
+ /usr/include/stdc-predef.h
