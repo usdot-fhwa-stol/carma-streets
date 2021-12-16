b1e2efd6214980dc5c71fe516a95930ce9db45fb
dan-du-car <dan.du@leidos.com>
2021-12-16 12:21:05 -0800

    update

--- message_services/test/test_mobilitypath_worker.cpp
+++ message_services/test/test_mobilitypath_worker.cpp
@@ -13,7 +13,7 @@ TEST(test_mobilitypath_worker, process_incoming_msg)
 {
     message_services::workers::mobilitypath_worker mobilitypath_w_obj;
     ASSERT_EQ(0, mobilitypath_w_obj.get_curr_map().size());
-    std::string mobilitypath_json_str = "{\"metadata\": {\"timestamp\" : \"1632679657\",\"hostStaticId\": \"DOT-507\",\"hostBSMId\": \"bsmXXXid\"}, \"trajectory\": { \"location\": {\"ecef_x\": 122, \"ecef_y\": 1,\"ecef_z\": 0}}}";
+    std::string mobilitypath_json_str = "{\"metadata\": {\"timestamp\" : \"1632679657\",\"hostStaticId\": \"DOT-507\",\"hostBSMId\": \"bsmXXXid\"}, \"trajectory\": { \"location\": {\"ecefX\": 122, \"ecef_y\": 1,\"ecefZ\": 0}}}";
     mobilitypath_w_obj.process_incoming_msg(mobilitypath_json_str);
     ASSERT_EQ(1632679657, mobilitypath_w_obj.get_curr_map().begin()->second.getHeader().timestamp);
     ASSERT_EQ("DOT-507", mobilitypath_w_obj.get_curr_map().begin()->second.getHeader().sender_id);
@@ -25,9 +25,9 @@ TEST(test_mobilitypath_worker, pop_cur_element_from_list)
 {
     message_services::workers::mobilitypath_worker mobilitypath_w_obj;
     ASSERT_EQ(0, mobilitypath_w_obj.get_curr_map().size());
-    std::string mobilitypath_json_str_1 = "{\"metadata\": {\"timestamp\" : \"1632679657\",\"hostStaticId\": \"DOT-507\",\"hostBSMId\": \"bsmXXXid\"}, \"trajectory\": { \"location\": {\"ecef_x\": 122, \"ecef_y\": 1,\"ecef_z\": 0,\"timestamp\": \"1632679657\"}}}";
-    std::string mobilitypath_json_str_2 = "{\"metadata\": {\"timestamp\" : \"1632679758\",\"hostStaticId\": \"DOT-508\",\"planId\" : \"00000000-0000-0000-0000-000000000000\",\"hostBSMId\": \"bsmXXXid\"}, \"trajectory\": { \"location\": {\"ecef_x\": 122, \"ecef_y\": 1,\"ecef_z\": 0,\"timestamp\": \"1632679657\"}, \"offsets\": [{\"offset_x\": 122, \"offset_y\": 1,\"offset_z\": 0}] }}";
-    std::string mobilitypath_json_str_3 = "{\"metadata\": {\"timestamp\" : \"1632679859\",\"hostStaticId\": \"DOT-509\" ,\"planId\" : \"00000000-0000-0000-0000-000000000000\",\"hostBSMId\": \"bsmXXXid\"}, \"trajectory\": { \"location\": {\"ecef_x\": 122, \"ecef_y\": 1,\"ecef_z\": 0,\"timestamp\": \"1632679657\"}, \"offsets\": [{\"offset_x\": 122, \"offset_y\": 1,\"offset_z\": 0}]}}";
+    std::string mobilitypath_json_str_1 = "{\"metadata\": {\"timestamp\" : \"1632679657\",\"hostStaticId\": \"DOT-507\",\"hostBSMId\": \"bsmXXXid\"}, \"trajectory\": { \"location\": {\"ecefX\": 122, \"ecefY\": 1,\"ecefZ\": 0,\"timestamp\": \"1632679657\"}}}";
+    std::string mobilitypath_json_str_2 = "{\"metadata\": {\"timestamp\" : \"1632679758\",\"hostStaticId\": \"DOT-508\",\"planId\" : \"00000000-0000-0000-0000-000000000000\",\"hostBSMId\": \"bsmXXXid\"}, \"trajectory\": { \"location\": {\"ecefX\": 122, \"ecefY\": 1,\"ecefZ\": 0,\"timestamp\": \"1632679657\"}, \"offsets\": [{\"offsetX\": 122, \"offsetY\": 1,\"offsetZ\": 0}] }}";
+    std::string mobilitypath_json_str_3 = "{\"metadata\": {\"timestamp\" : \"1632679859\",\"hostStaticId\": \"DOT-509\" ,\"planId\" : \"00000000-0000-0000-0000-000000000000\",\"hostBSMId\": \"bsmXXXid\"}, \"trajectory\": { \"location\": {\"ecefX\": 122, \"ecefY\": 1,\"ecefZ\": 0,\"timestamp\": \"1632679657\"}, \"offsets\": [{\"offsetX\": 122, \"offsetY\": 1,\"offsetZ\": 0}]}}";
     mobilitypath_w_obj.process_incoming_msg(mobilitypath_json_str_1);
     mobilitypath_w_obj.process_incoming_msg(mobilitypath_json_str_2);
     mobilitypath_w_obj.process_incoming_msg(mobilitypath_json_str_3);
@@ -39,6 +39,7 @@ TEST(test_mobilitypath_worker, pop_cur_element_from_list)
     ASSERT_EQ("bsmXXXid", mobilitypath_w_obj.get_curr_map().begin()->second.getHeader().sender_bsm_id);
     ASSERT_EQ("", mobilitypath_w_obj.get_curr_map().begin()->second.getHeader().recipient_id);
     ASSERT_EQ(1632679758, mobilitypath_w_obj.get_curr_map().begin()->second.getHeader().timestamp);
+    std::cout << mobilitypath_w_obj.get_curr_map().begin()->second.getTrajectory().offsets.front().offset_x;
     ASSERT_EQ(122, mobilitypath_w_obj.get_curr_map().begin()->second.getTrajectory().offsets.begin()->offset_x);
     ASSERT_EQ(1, mobilitypath_w_obj.get_curr_map().begin()->second.getTrajectory().offsets.begin()->offset_y);
     ASSERT_EQ(0, mobilitypath_w_obj.get_curr_map().begin()->second.getTrajectory().offsets.begin()->offset_z);
