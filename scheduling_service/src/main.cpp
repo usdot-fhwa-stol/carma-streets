e8d31a85c8ee0841ba718318ae55eea4c084e40d
dan-du-car <62157949+dan-du-car@users.noreply.github.com>
2021-12-29 16:07:52 -0500

    Security hotspot issue (#68)
    * init
    
    * update

--- scheduling_service/src/main.cpp
+++ scheduling_service/src/main.cpp
@@ -460,7 +460,7 @@ void call_consumer_thread()
 
             const char* paylod= consumer_worker->consume(1000);
 
-            if(strlen(paylod) > 0)
+            if(paylod != nullptr && strlen(paylod) > 0)
             {
 
                 //spdlog::info("Consumed message payload: {0}", paylod);   
