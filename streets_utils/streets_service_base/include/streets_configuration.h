54cdfa603af91d852f392d99e06fdafc6f0ae446
Paul K Bourelly <paul.k.bourelly@leidos.com>
2022-03-22 14:46:02 -0400

    Issu-115:Sonar-Scanner code smells

--- streets_utils/streets_service_base/include/streets_configuration.h
+++ streets_utils/streets_service_base/include/streets_configuration.h
@@ -68,59 +68,53 @@ struct configuration {
      * @param val rapidjson::Value object representing configuration.
      */ 
     void fromJson(const rapidjson::Value &val ){
-        if ( val.HasMember("name") ) {
-            name = val.FindMember("name")->value.GetString();
-        }else {
+
+        if (!val.FindMember("name")->value.IsString()) {
             throw streets_configuration_exception("Configuration is missing name!");
         }
+        name = val.FindMember("name")->value.GetString();
 
-        if ( val.HasMember("description") ){
-            description = val.FindMember("description")->value.GetString();
-        } else {
+        if ( !val.FindMember("description")->value.IsString() ){
             throw streets_configuration_exception("Configuration is" + name + " missing a description");
-        }
+        } 
+        description = val.FindMember("description")->value.GetString();
 
-        if ( val.HasMember("type") ){
+        if ( val.FindMember("type")->value.IsString() ){
             std::string doc_type = val.FindMember("type")->value.GetString();
             if ( doc_type == "STRING" ) {
                 type = data_type::config_string;
-                if ( val.HasMember("value") && val.FindMember("value")->value.IsString() ) {
-                    value = val.FindMember("value")->value.GetString();
-                }
-                else {
+                if ( !val.FindMember("value")->value.IsString() ) {
                     throw streets_configuration_exception("Configuration is missing/incorrect a value");
                 }
+                value = val.FindMember("value")->value.GetString();
             }
             else if (doc_type == "INTEGER"){
                 type = data_type::config_int;
-                if ( val.HasMember("value") && val.FindMember("value")->value.IsInt() ) {
-                    value = std::to_string(val.FindMember("value")->value.GetInt());
-                }
-                else {
+                if ( !val.FindMember("value")->value.IsInt() ) {
                     throw streets_configuration_exception("Configuration is missing/incorrect a value");
                 }
+                value = std::to_string(val.FindMember("value")->value.GetInt());
             }
             else if (doc_type == "DOUBLE"){
                 type = data_type::config_double;
-                if ( val.HasMember("value") && val.FindMember("value")->value.IsDouble() ) {
-                    value = std::to_string(val.FindMember("value")->value.GetDouble());
-                }
-                else {
+                if ( !val.FindMember("value")->value.IsDouble() ) {
                     throw streets_configuration_exception("Configuration is missing/incorrect a value");
                 }
+                value = std::to_string(val.FindMember("value")->value.GetDouble());
             }
             else if (doc_type == "BOOL") {
                 type = data_type::config_bool;
-                if ( val.HasMember("value") && val.FindMember("value")->value.IsBool() ) {
-                    value = val.FindMember("value")->value.GetBool() ? "true" : "false";
-                }
-                else {
+                if ( !val.FindMember("value")->value.IsBool() ) {
                     throw streets_configuration_exception("Configuration is missing/incorrect a value");
                 }
-            } else {
+                value = val.FindMember("value")->value.GetBool() ? "true" : "false";
+                
+            } 
+            else {
                 throw streets_configuration_exception("Data type configuration is invalid!");
             }
-        } else {
+        } 
+        else {
             throw streets_configuration_exception("Configuration " + name + " is missing a type");
         }
         
