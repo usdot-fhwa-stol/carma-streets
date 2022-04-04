fb9fa8ca0cecf31d53566863f3c2df26c24fd579
Paul K Bourelly <paul.k.bourelly@leidos.com>
2022-04-04 13:50:25 -0400

    Issue-126:Refactor kafka_clients to use streets_service_base

--- kafka_clients/src/main.cpp
+++ kafka_clients/src/main.cpp
@@ -1,13 +1,12 @@
 #include "kafka_client.h"
+#include "streets_configuration.h"
 
 void call_consumer_thread()
 {
     auto client = std::make_shared<kafka_clients::kafka_client>();
-    std::string file_name = "../manifest.json";
-    rapidjson::Document doc_json = client->read_json_file(file_name);
-    std::string bootstrap_server = client->get_value_by_doc(doc_json, "BOOTSTRAP_SERVER");
-    std::string group_id = client->get_value_by_doc(doc_json, "GROUP_ID");
-    std::string topic = client->get_value_by_doc(doc_json, "CONSUMER_TOPIC");
+    std::string bootstrap_server = streets_service::streets_configuration::get_string_config( "BOOTSTRAP_SERVER");
+    std::string group_id = streets_service::streets_configuration::get_string_config("GROUP_ID");
+    std::string topic = streets_service::streets_configuration::get_string_config("CONSUMER_TOPIC");
     auto consumer_worker = client->create_consumer(bootstrap_server, topic, group_id);
     if (!consumer_worker->init())
     {
@@ -38,10 +37,8 @@ void call_consumer_thread()
 void call_producer_thread()
 {
     auto client = std::make_shared<kafka_clients::kafka_client>();
-    std::string file_name = "../manifest.json";
-    rapidjson::Document doc_json = client->read_json_file(file_name);
-    std::string bootstrap_server = client->get_value_by_doc(doc_json, "BOOTSTRAP_SERVER");
-    std::string topic = client->get_value_by_doc(doc_json, "PRODUCER_TOPIC");
+    std::string bootstrap_server = streets_service::streets_configuration::get_string_config("BOOTSTRAP_SERVER");
+    std::string topic = streets_service::streets_configuration::get_string_config("PRODUCER_TOPIC");
     auto producer_worker = client->create_producer(bootstrap_server, topic);
     if (!producer_worker->init())
     {
@@ -66,6 +63,7 @@ void call_producer_thread()
 
 int main(int argc, char **argv)
 {
+    streets_service::streets_configuration::initialize_logger();
     boost::thread t_producer{call_producer_thread};
     boost::thread t_consumer{call_consumer_thread};
     t_producer.join();
