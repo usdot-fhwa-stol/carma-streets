fdbb73f689e688b4beefea1201ffec4ee8211445
Anish <Anishdeva17@gmail.com>
2022-06-16 13:52:57 -0400

    add method description

--- tsc_client_service/include/snmp_client.h
+++ tsc_client_service/include/snmp_client.h
@@ -47,12 +47,28 @@ class TrafficSignalControllerService
         int snmp_version_ = 0;
 
     public:
+        /** @brief Constructor for Traffic Signal Controller Service client.
+         * @param ip The ip ,as a string, for the host to establish snmp communication with.
+         * @param port Target port as integer on the host for snmp communication.
+         * @param community The community id as a string. Defaults to "public" if unassigned.
+         * @param community_len The length of the community id. Defaults to "6" if unassigned.
+         * @param snmp_version The snmp_version as defined in net-snmp.Default to 0 if unassigned.
+         *                      net-snmp version definition: SNMP_VERSION_1:0 SNMP_VERSION_2c:1 SNMP_VERSION_2u:2 SNMP_VERSION_3:3"
+         * @param timeout The time in microseconds after which an snmp session request expires.**/
         TrafficSignalControllerService(std::string ip, int port, std::string community = "public", int community_len = 6, int snmp_version = 0, int timeout = 100);
         
+        /** @brief Destructor for client.**/
         ~TrafficSignalControllerService();
-
+        
+        /** @brief Returns integer value for input OID at the Traffic Signal Controller.
+         *  @param input_oid The OID to request information for.
+         *  @return Integer value at the oid, returns -1 if value cannot be requested or oid doesn't have an integer value to return.*/
         int process_snmp_get_request(std::string input_oid);
 
+        /** @brief Sets integer value at input OID at the Traffic Signal Controller.
+         *  @param input_oid The OID to set value for.
+         *  @param value The value to set.
+         *  @return boolean for whether value could be set, returns true if successful and false if value cannot be set.*/
         bool process_snmp_set_request(std::string input_oid, int value);
 
         // Values from config
@@ -60,7 +76,6 @@ class TrafficSignalControllerService
         int port_ = 0;
         std::string community_ = "public";
         int community_len_ = 3;
-        
         int timeout_ = 10000;
 
 };
