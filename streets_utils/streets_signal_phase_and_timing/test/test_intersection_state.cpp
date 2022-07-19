071964625f7465b98fde177cce303b4ad7068282
dan-du-car <62157949+dan-du-car@users.noreply.github.com>
2022-07-19 09:27:46 -0400

    Add Utility Methods to streets_signal_phase_and_timing library for unit translation from J2735 (#166)
    * init
    
    * init
    
    * use moy
    
    * address CI
    
    * address CI
    
    * code smell

---
+++ streets_utils/streets_signal_phase_and_timing/test/test_intersection_state.cpp
@@ -0,0 +1,95 @@
+#include <gtest/gtest.h>
+#include <spdlog/spdlog.h>
+#include <rapidjson/writer.h>
+#include <rapidjson/stringbuffer.h>
+#include "spat.h"
+#include "signal_phase_and_timing_exception.h"
+
+using namespace signal_phase_and_timing;
+
+class test_intersection_state : public ::testing::Test
+{
+public:
+    intersection_state iss;
+    uint32_t moy;     // Minute of the year
+    uint16_t msOfMin; // Millisecond of the minute
+    uint64_t epoch_timestamp; //Current epoch timestamp
+    uint64_t epoch_intersection_state_timestamp; //Epoch minutes + intersection state timestamp
+
+protected:
+    void SetUp() override
+    {
+        std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
+        time_t tt = std::chrono::system_clock::to_time_t(now);
+        tm utctime;
+        gmtime_r(&tt, &utctime);
+
+        // Calculate the minute of the year
+        moy = utctime.tm_min + (utctime.tm_hour * 60) + (utctime.tm_yday * 24 * 60);
+        iss.moy = moy;    
+
+        // Calculate the millisecond of the minute
+        std::chrono::milliseconds epochMs = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch());
+        epoch_timestamp = epochMs.count();
+        SPDLOG_INFO("epoch_timestamp {0}", epoch_timestamp );        
+        std::chrono::seconds epochS = std::chrono::duration_cast<std::chrono::seconds>(epochMs);
+        msOfMin = 1000 * (epochS.count() % 60) + (epochMs.count() % 1000);
+        iss.time_stamp = msOfMin;
+        SPDLOG_INFO("Millisecond of the minute {0}", msOfMin );        
+
+        // Get the epoch minutes 
+        std::chrono::minutes epochMin = std::chrono::duration_cast<std::chrono::minutes>(now.time_since_epoch());
+        SPDLOG_INFO("Epoch minutes {0}", epochMin.count());
+
+        // Epoch minutes + milliseconds of current minute
+        epoch_intersection_state_timestamp = epochMin.count() * iss.MIN_TO_SECONDS*iss.SECOND_TO_MILLISECONDS + msOfMin;
+        SPDLOG_INFO("epoch_intersection_state_timestamp {0}", epoch_intersection_state_timestamp );
+    }
+};
+
+TEST_F(test_intersection_state, fromJson)
+{
+    rapidjson::Value json(rapidjson::kObjectType);
+    rapidjson::Document doc;
+    auto allocator = doc.GetAllocator();
+    json.AddMember("time_stamp", 1000, allocator);
+    json.AddMember("id", 1, allocator);
+    json.AddMember("revision", 1, allocator);
+    json.AddMember("moy", 12, allocator);
+    json.AddMember("status", "1", allocator);
+    rapidjson::Value states_list(rapidjson::kArrayType);
+    json.AddMember("states", states_list, allocator);
+
+    intersection_state iss;
+    iss.fromJson(json);
+    ASSERT_EQ(1000, iss.time_stamp);
+}
+
+TEST_F(test_intersection_state, toJson)
+{
+    intersection_state iss;
+    rapidjson::Document doc;
+    auto allocator = doc.GetAllocator();
+    iss.time_stamp = 1000;
+    iss.id= 1;
+    iss.revision = 1;
+    iss.moy= 123;
+    iss.status = "1";
+    movement_state mms;
+    iss.states.push_back(mms);
+    auto json = iss.toJson(allocator);
+
+    ASSERT_EQ(1000, json["time_stamp"].GetInt());
+}
+
+TEST_F(test_intersection_state, convert_min_mills2epoch_ts)
+{
+    ASSERT_EQ(epoch_timestamp, iss.convert_min_mills2epoch_ts(moy, iss.time_stamp));
+    ASSERT_EQ(epoch_intersection_state_timestamp, iss.convert_min_mills2epoch_ts(moy, iss.time_stamp));
+}
+
+TEST_F(test_intersection_state, get_epoch_timestamp)
+{
+    ASSERT_EQ(epoch_timestamp, iss.get_epoch_timestamp());
+    ASSERT_EQ(epoch_intersection_state_timestamp, iss.get_epoch_timestamp());
+}
---
+++ streets_utils/streets_signal_phase_and_timing/test/test_time_change_detail.cpp
@@ -0,0 +1,131 @@
+#include <gtest/gtest.h>
+#include <spdlog/spdlog.h>
+#include <rapidjson/writer.h>
+#include <rapidjson/stringbuffer.h>
+#include "spat.h"
+#include "signal_phase_and_timing_exception.h"
+
+using namespace signal_phase_and_timing;
+
+class test_time_change_detail : public ::testing::Test
+{
+public:
+    time_change_details tcd;
+    uint64_t epoch_start_time;
+    uint64_t epoch_min_end_time;
+    uint64_t epoch_max_end_time;
+    uint64_t epoch_next_time;
+    int THREE_SECONDS = 3;
+    int FIVE_SECONDS = 5;
+    int TEN_SECONDS = 10;
+protected:    
+    void SetUp() override
+    {
+        typedef std::chrono::duration<int, std::ratio_multiply<std::chrono::hours::period, std::ratio<24>>::type> days;
+        auto tp = std::chrono::system_clock::now();
+        auto duration = tp.time_since_epoch();
+        SPDLOG_INFO("epoch time point: {0}", tp.time_since_epoch().count());
+
+        days d = std::chrono::duration_cast<days>(duration);
+        SPDLOG_INFO("epoch days: {0}", d.count());
+
+        auto cur_h = std::chrono::duration_cast<std::chrono::hours>(duration - d);
+        SPDLOG_INFO("current hours: {0}", cur_h.count());
+
+        auto cur_m = std::chrono::duration_cast<std::chrono::minutes>(duration - d - cur_h);
+        SPDLOG_INFO("current minutes: {0}", cur_m.count());
+
+        auto cur_s = std::chrono::duration_cast<std::chrono::seconds>(duration - d - cur_h - cur_m);
+        SPDLOG_INFO("current seconds: {0}", cur_s.count());
+
+        auto cur_ms = std::chrono::duration_cast<std::chrono::milliseconds>(duration - d - cur_h - cur_m - cur_s);
+        SPDLOG_INFO("current milliseconds: {0}", cur_ms.count());
+
+        //Start time
+        double fractionSeconds = cur_s.count() + (cur_ms.count() / 1000.0);
+        long tenth_seconds_at_current_hour = ((cur_m.count() * 60) + fractionSeconds) * 10;
+        SPDLOG_INFO("Tenth seconds at current hour: {0}", tenth_seconds_at_current_hour);
+        tcd.start_time =  tenth_seconds_at_current_hour;
+
+        auto hours_since_epoch = std::chrono::duration_cast<std::chrono::hours>(duration).count();
+        auto hours_sicne_epoch_in_milliseconds = hours_since_epoch * tcd.HOUR_TO_SECONDS * tcd.SECOND_TO_MILLISECONDS;
+        epoch_start_time = hours_sicne_epoch_in_milliseconds + tcd.start_time * 100;
+        SPDLOG_INFO("Epoch start time: {0}", epoch_start_time);
+
+        //Min end time in THREE_SECONDS
+        tcd.min_end_time = tenth_seconds_at_current_hour + THREE_SECONDS * 10;
+        epoch_min_end_time = hours_sicne_epoch_in_milliseconds + tcd.min_end_time * 100;
+        SPDLOG_INFO("Epoch min end time (in THREE SECONDS): {0}", epoch_min_end_time);
+
+        //Max end time in FIVE_SECONDS
+        tcd.max_end_time = tenth_seconds_at_current_hour + FIVE_SECONDS * 10;
+        epoch_max_end_time = hours_sicne_epoch_in_milliseconds + tcd.max_end_time * 100;
+        SPDLOG_INFO("Epoch max end time (in FIVE SECONDS): {0}", epoch_max_end_time);
+
+        //Next time in TEN_SECONDS
+        tcd.next_time = tenth_seconds_at_current_hour + TEN_SECONDS * 10;
+        epoch_next_time = hours_sicne_epoch_in_milliseconds +  tcd.next_time * 100;
+        SPDLOG_INFO("Epoch next time (in TEN SECONDS): {0}", epoch_next_time);
+
+    }
+};
+
+TEST_F(test_time_change_detail, fromJson)
+{
+    rapidjson::Value json(rapidjson::kObjectType);
+    rapidjson::Document doc;
+    auto allocator = doc.GetAllocator();
+    json.AddMember("start_time", 1000, allocator);
+    json.AddMember("min_end_time", 2000, allocator);
+    json.AddMember("max_end_time", 3000, allocator);
+    json.AddMember("next_time", 4000, allocator);
+
+    time_change_details tcd;
+    tcd.fromJson(json);
+    ASSERT_EQ(1000, tcd.start_time);
+    ASSERT_EQ(2000, tcd.min_end_time);
+    ASSERT_EQ(3000, tcd.max_end_time);
+    ASSERT_EQ(4000, tcd.next_time);
+}
+
+TEST_F(test_time_change_detail, toJson)
+{
+    time_change_details tcd;
+    rapidjson::Document doc;
+    auto allocator = doc.GetAllocator();
+    tcd.start_time = 1000;
+    tcd.min_end_time = 2000;
+    tcd.max_end_time = 3000;
+    tcd.next_time = 4000;
+    auto json = tcd.toJson(allocator);
+
+    ASSERT_EQ(1000, json["start_time"].GetInt());
+    ASSERT_EQ(2000, json["min_end_time"].GetInt());
+    ASSERT_EQ(3000, json["max_end_time"].GetInt());
+    ASSERT_EQ(4000, json["next_time"].GetInt());
+}
+
+TEST_F(test_time_change_detail, convert_hour_mills2epoch_ts)
+{
+    ASSERT_EQ(epoch_start_time, tcd.convert_hour_tenth_secs2epoch_ts(tcd.start_time));
+}
+
+TEST_F(test_time_change_detail, get_epoch_start_time)
+{
+    ASSERT_EQ(epoch_start_time, tcd.get_epoch_start_time());
+}
+
+TEST_F(test_time_change_detail, get_epoch_min_end_time)
+{
+    ASSERT_EQ(epoch_min_end_time, tcd.get_epoch_min_end_time());
+}
+
+TEST_F(test_time_change_detail, get_epoch_max_end_time)
+{
+    ASSERT_EQ(epoch_max_end_time, tcd.get_epoch_max_end_time());
+}
+
+TEST_F(test_time_change_detail, get_epoch_next_time)
+{
+    ASSERT_EQ(epoch_next_time, tcd.get_epoch_next_time());
+}
