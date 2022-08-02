94b5b785911054c635f91d44248b53d978c0a348
Anish <Anishdeva17@gmail.com>
2022-08-02 12:29:43 -0400

    fix

--- tsc_client_service/include/monitor_tsc_state.h
+++ tsc_client_service/include/monitor_tsc_state.h
@@ -16,11 +16,7 @@ namespace traffic_signal_controller_service
         int min_green;
         /*Maximum green durations for each active vehicle phase in miiliseconds*/
         int max_green;
-<<<<<<< HEAD
         /*Default Green duration for phase in miiliseconds*/
-=======
-        /*Default Green duration for phase in milliseconds*/
->>>>>>> develop
         int green_duration;
         /*Yellow signal duration for phase in miiliseconds*/
         int yellow_duration;
@@ -108,45 +104,6 @@ namespace traffic_signal_controller_service
         * **/
         int get_red_duration(int phase_num);
 
-<<<<<<< HEAD
-=======
-        /** 
-         * @brief Method for getting maximum channels for the traffic signal controller
-         * @return number of maximum channels in the traffic signal controller
-        **/
-        int get_max_channels() const;
-
-        /** @brief Get minimum green time for a phase
-        ** @param phase_num The phase for which the min green needs to be requested
-        ** @return minimum green time in milliseconds
-        * **/
-        int get_min_green(int phase_num) const;
-
-        /** @brief Get maximum green time for a phase
-        ** @param phase_num The phase for which the maximum green needs to be requested
-        ** @return maximum green time in milliseconds
-        * **/
-        int get_max_green(int phase_num) const;
-
-        /** @brief Get yellow duration time for a phase
-        ** @param phase_num The phase for which the yellow duration needs to be requested
-        ** @return yellow duration time in milliseconds
-        * **/
-        int get_yellow_duration(int phase_num) const;
-
-        /** @brief Get red clearance time for a phase
-        ** @param phase_num The phase for which the red clearance needs to be requested
-        ** @return red clearance time in milliseconds
-        * **/
-        int get_red_clearance(int phase_num) const;
-
-        /** @brief Get red duration for a phase
-        ** @param phase_num The phase for which the red duration needs to be requested
-        ** @return red duration time in milliseconds
-        * **/
-        int get_red_duration(int phase_num);
-
->>>>>>> develop
         /** @brief Get a sequence of phases following the given phase
         ** @param phase_num The phase for which the sequence needs to be obtained
         ** @return a vector as a sequence of phases starting from the given phase
