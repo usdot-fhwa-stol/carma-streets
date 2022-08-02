5259fbe61b3ea0acd2002a29e1d8c08dac3854db
paulbourelly999 <paul.k.bourelly@leidos.com>
2022-08-02 16:55:09 -0700

    Issue-173:Adding tsc_service test case

--- tsc_client_service/test/test_tsc_service.cpp
+++ tsc_client_service/test/test_tsc_service.cpp
@@ -4,6 +4,8 @@
 using namespace traffic_signal_controller_service; 
 
 TEST(test_tsc_service, test_initialization) {
-    // tsc_service service;
-    // ASSERT_FALSE(service.initialize());
+    tsc_service service;
+    ASSERT_FALSE(service.initialize());
+    // Allow for destructor to run before other test cases
+    sleep(1);
 }
--- tsc_client_service/test/test_udp_socket_listener.cpp
+++ tsc_client_service/test/test_udp_socket_listener.cpp
@@ -16,7 +16,7 @@ namespace traffic_signal_controller_service
     TEST(udp_socket_listener_test, test_create_socket)
     {
         std::string tsc_ip_bad = "192.168.120.51";
-        int tsc_port = 6053;
+        int tsc_port = 6055;
         int tsc_timeout = 3;
 
         udp_socket_listener listener(tsc_ip_bad, tsc_port, tsc_timeout);
@@ -31,7 +31,7 @@ namespace traffic_signal_controller_service
     TEST(udp_socket_listener_test, test_create_socket_timeout)
     {
         std::string tsc_ip= "127.0.0.1";
-        int tsc_port = 6052;
+        int tsc_port = 6055;
         int tsc_timeout = 2;
 
         udp_socket_listener listener(tsc_ip, tsc_port, tsc_timeout);
@@ -40,7 +40,6 @@ namespace traffic_signal_controller_service
             listener.receive();
         }
         catch( const udp_socket_listener_exception &ex){
-            SPDLOG_INFO("Catching correct exception");
             ASSERT_STREQ( ex.what(), "Timeout of 2 seconds has elapsed. Closing SPaT Work UDP Socket");
         }
         catch( ... ) {
@@ -56,7 +55,7 @@ namespace traffic_signal_controller_service
     TEST(udp_socket_listener_test, test_invalid_ip)
     {
         std::string tsc_ip_bad = "asdhas.asd";
-        int tsc_port = 6053;
+        int tsc_port = 6055;
         int tsc_timeout = 2;
 
         udp_socket_listener listener(tsc_ip_bad, tsc_port, tsc_timeout);
