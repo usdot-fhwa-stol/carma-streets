c36bc43ea40bb88bd83d5e0a4ae4240710aa25e4
paulbourelly999 <paul.k.bourelly@leidos.com>
2022-08-04 05:38:32 -0700

    Issue-173:Added unit test

--- tsc_client_service/test/test_spat_worker.cpp
+++ tsc_client_service/test/test_spat_worker.cpp
@@ -4,7 +4,7 @@
 
 using namespace traffic_signal_controller_service;
 
-TEST(test_spat_worker, test_initialization) {
+TEST(test_spat_worker, test_receive_timeout) {
     spat_worker worker("127.0.0.1", 67921, 2, false);
     auto spat_ptr = std::make_shared<signal_phase_and_timing::spat>();
     ASSERT_TRUE( worker.initialize());
@@ -18,5 +18,12 @@ TEST(test_spat_worker, test_initialization) {
     catch( ... ) {
         GTEST_FAIL();
     }
+
+}
+
+TEST(test_spat_worker, test_initialization) {
+    spat_worker worker("192.168.120.51", 67921, 2, false);
+    auto spat_ptr = std::make_shared<signal_phase_and_timing::spat>();
+    ASSERT_FALSE( worker.initialize());
 
 }
