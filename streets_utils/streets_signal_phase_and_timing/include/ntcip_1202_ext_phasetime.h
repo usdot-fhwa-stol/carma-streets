159d48296817503b2affdf9eb74477f063027fc1
paulbourelly999 <paul.k.bourelly@leidos.com>
2022-08-08 07:03:02 -0700

    Issue-173: Update ntcip_1202_phastime comments

--- streets_utils/streets_signal_phase_and_timing/include/ntcip_1202_ext_phasetime.h
+++ streets_utils/streets_signal_phase_and_timing/include/ntcip_1202_ext_phasetime.h
@@ -7,12 +7,37 @@ namespace ntcip {
 
     struct ntcip_1202_ext_phasetime
     {
+        /** @brief Phase number (NTCIP) of phase. */
         uint8_t phase_number = 0;
+        /** @brief Time offset in deciseconds to min end time elapse for vehicle phase. 
+         * DO NOT use member directly, stored in Network Byte Order. Use getter to intpret
+         * correctly.
+         */
         uint16_t spat_veh_min_time_to_change = 0;
+        /** @brief Time offset in deciseconds to max end time elapse for vehicle phase. 
+         * DO NOT use member directly, stored in Network Byte Order. Use getter to intpret
+         * correctly.
+         */
         uint16_t spat_veh_max_time_to_change = 0;
+        /** @brief Time offset in deciseconds to min end time elapse for pedestrian phase. 
+         * DO NOT use member directly, stored in Network Byte Order. Use getter to intpret
+         * correctly.
+         */
         uint16_t spat_ped_min_time_to_change = 0;
+        /** @brief Time offset in deciseconds to max end time elapse for pedestrian phase. 
+         * DO NOT use member directly, stored in Network Byte Order. Use getter to intpret
+         * correctly.
+         */
         uint16_t spat_ped_max_time_to_change = 0;
+        /** @brief Time offset in deciseconds to min end time elapse for overlap. 
+         * DO NOT use member directly, stored in Network Byte Order. Use getter to intpret
+         * correctly.
+         */
         uint16_t spat_ovlp_min_time_to_change = 0;
+        /** @brief Time offset in deciseconds to max end time elapse for overlap. 
+         * DO NOT use member directly, stored in Network Byte Order. Use getter to intpret
+         * correctly.
+         */
         uint16_t spat_ovp_max_time_to_change = 0;
         /**
          * @brief Get spat_veh_min_time_to_change and convert from 
