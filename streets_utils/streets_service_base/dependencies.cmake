fd6bc41db83f7366c8d40c66a4f9214dca6d06b5
Adam Morrissett <me@adamlm.com>
2022-10-13 22:19:18 -0400

    streets_service_base: change RapidJSON dependency
    The RapidJSON library was previously built from source as a subproject
    using FetchContent. However, while testing installation to non-standard
    locations, it was discovered that overriding RapidJSON's install
    location is not easy (if you want to do it properly). RapidJSON build
    architecture does not seem conducive to subprojects, so it is probably
    better to let the developer handle its building/installation.
    
    Note: We should add a notice to builders that they need to install
    RapidJSON from source using a version beyond a specific commit. This is
    because the streets_service_base uses functionality not found in the
    latest release.

--- streets_utils/streets_service_base/dependencies.cmake
+++ streets_utils/streets_service_base/dependencies.cmake
@@ -1,25 +1,4 @@
-include(FetchContent)
-
-FetchContent_Declare(rapidjson
-    GIT_REPOSITORY https://github.com/Tencent/rapidjson.git
-    GIT_TAG 06d58b9e848c650114556a23294d0b6440078c61  # Top-level project requires post 1.1.0 (latest release) features
-)
-
-FetchContent_GetProperties(rapidjson)
-if(NOT rapidjson_POPULATED)
-    FetchContent_Populate(rapidjson)
-
-    # RapidJSON builds GTest by source, which conflicts with the source build in the top-level project. Since we do not
-    # need to test RapidJSON, we disable tests, examples, and docs.
-
-    set(RAPIDJSON_BUILD_DOC OFF CACHE INTERNAL "")
-    set(RAPIDJSON_BUILD_EXAMPLES OFF CACHE INTERNAL "")
-    set(RAPIDJSON_BUILD_TESTS OFF CACHE INTERNAL "")
-    set(RAPIDJSON_BUILD_THIRDPARTY_GTEST OFF CACHE INTERNAL "")
-
-    add_subdirectory(${rapidjson_SOURCE_DIR} ${rapidjson_BINARY_DIR})
-endif()
-
+find_package(RapidJSON REQUIRED)
 find_package(Boost 1.71.0 EXACT COMPONENTS system filesystem thread REQUIRED)
 find_package(spdlog 1.10.0 EXACT REQUIRED)
 find_package(GTest REQUIRED MODULE)
