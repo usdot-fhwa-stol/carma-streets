3b71bf2de1b8eac324820806764dd89cb4f0e75c
Adam Morrissett <me@adamlm.com>
2022-10-16 19:22:43 -0400

    streets_service_base: Make GTest conditional
    If the package does not build unit tests, then GTest is not actually
    required. However, the CMake configuration previously required GTest in
    all instances. Now, requiring GTest is conditioned on tests being built.
    
    streets_utils/streets_service_base/dependencies.cmake
    * make GTest being required conditioned on unit tests being built

--- streets_utils/streets_service_base/dependencies.cmake
+++ streets_utils/streets_service_base/dependencies.cmake
@@ -1,4 +1,7 @@
 find_package(RapidJSON REQUIRED)
 find_package(Boost 1.71.0 EXACT COMPONENTS system filesystem thread REQUIRED)
 find_package(spdlog 1.10.0 EXACT REQUIRED)
-find_package(GTest REQUIRED MODULE)
+
+if(STREETSSERVICEBASE_ENABLE_TESTING OR PROJECT_IS_TOP_LEVEL)
+  find_package(GTest REQUIRED)
+endif()
