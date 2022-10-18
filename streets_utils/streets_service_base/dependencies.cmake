5b1a4c7a27f5e5585ef0491cda081be4498faa7f
Adam Morrissett <me@adamlm.com>
2022-10-17 22:28:43 -0400

    streets_service_base: Revert CMake & Boost version
    CARMA Streets uses Ubuntu 18.04 Docker containers, which only have CMake
    version 3.10.2 and Boost version 1.65. Therefore, the versions
    previously required need to be reverted back so that they are compatible
    with the production environment.
    
    streets_utils/streets_service_base/CMakeLists.txt
    * revert CMake version to 3.10.2 instead of 3.16
    
    streets_utils/streets_service_base/dependencies.cmake
    * remove Boost version requirement. Some minimum version should be
      established in the future.

--- streets_utils/streets_service_base/dependencies.cmake
+++ streets_utils/streets_service_base/dependencies.cmake
@@ -1,5 +1,5 @@
 find_package(RapidJSON REQUIRED)
-find_package(Boost 1.71.0 EXACT COMPONENTS system filesystem thread REQUIRED)
+find_package(Boost COMPONENTS system filesystem thread REQUIRED)
 find_package(spdlog 1.10.0 EXACT REQUIRED)
 
 if(STREETSSERVICEBASE_ENABLE_TESTING OR PROJECT_IS_TOP_LEVEL)
