bf5e54cf62859d8d54999f8823308ba24cd14bf6
Anish <Anishdeva17@gmail.com>
2022-10-25 08:46:48 -0400

    fix operator

--- streets_utils/streets_desired_phase_plan/include/streets_desired_phase_plan.h
+++ streets_utils/streets_desired_phase_plan/include/streets_desired_phase_plan.h
@@ -22,7 +22,7 @@ namespace streets_desired_phase_plan
 
         inline bool operator!=(const signal_group2green_phase_timing& obj2) const
         {
-            return start_time != obj2.start_time || end_time != obj2.end_time || signal_groups == obj2.signal_groups;
+            return start_time != obj2.start_time || end_time != obj2.end_time || signal_groups != obj2.signal_groups;
         }
     };
 
