3e4ac90c0f6c28aa6a41c3de19ca8920dabfa0dd
paulbourelly999 <paul.k.bourelly@leidos.com>
2022-12-01 09:32:31 -0800

    Added script for collecting/clearing logs

---
+++ collect_service_logs.sh
@@ -0,0 +1,11 @@
+# 
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#  License for the specific language governing permissions and limitations under
+#  the License.
+
+# script collects all service logs files and zips them into an archive.
+set -e
+zip -r service_logs.zip ./scheduling_service/logs ./intersection_model/logs ./tsc_client_service/logs ./message_services/logs ./signal_opt_service/logs
+sudo rm ./scheduling_service/logs/* ./intersection_model/logs/* ./tsc_client_service/logs/* ./message_services/logs/* ./signal_opt_service/logs/*
