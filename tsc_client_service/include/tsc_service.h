c91fcc1e7e3fa2172e0ec0cfe37a4cb75697c348
Anish <Anishdeva17@gmail.com>
2022-12-22 16:27:24 -0500

    fix

--- tsc_client_service/include/tsc_service.h
+++ tsc_client_service/include/tsc_service.h
@@ -245,7 +245,7 @@ namespace traffic_signal_controller_service {
              **/
             void set_tsc_hold_and_omit();
 
-            void configure_csv_logger() const
+            void configure_csv_logger() const;
 
     };
 }
