fe8060dfe3370bff89be492abd2a9048d97f0655
paulbourelly999 <paul.k.bourelly@leidos.com>
2023-04-12 23:58:50 -0400

    Fixed httpqengine install

--- build_scripts/install_rest_server_dependencies.sh
+++ build_scripts/install_rest_server_dependencies.sh
@@ -7,11 +7,18 @@ set -e
 apt-get update
 
 # NOTE: libwebsockets-dev from Ubuntu 20 on is sufficient
-DEPENDENCIES="libqhttpengine-dev \
-    libssl-dev \
+DEPENDENCIES="libssl-dev \
     qtbase5-dev \
     qtbase5-dev-tools"
     
 # install all things needed for deployment, always done
 apt-get install -y $DEPENDENCIES 
 
+cd /home/carma-streets/ext
+git clone https://github.com/etherealjoy/qhttpengine.git
+cd /home/carma-streets/ext/qhttpengine/
+mkdir build
+cd /home/carma-streets/ext/qhttpengine/build
+cmake .. 
+make -j
+make install
