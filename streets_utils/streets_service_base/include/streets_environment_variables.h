194ddf615972ed407251a4201a6a8ec009be7d35
dev <paul.k.bourelly@leidos.com>
2023-09-14 14:21:03 -0400

    PR update

--- streets_utils/streets_service_base/include/streets_environment_variables.h
+++ streets_utils/streets_service_base/include/streets_environment_variables.h
@@ -4,21 +4,21 @@
 
 namespace streets_service {
 
-    inline static const std::string LOGS_DIRECTORY_ENV = "LOGS_DIRECTORY";
+    inline const std::string LOGS_DIRECTORY_ENV = "LOGS_DIRECTORY";
 
-    inline static const std::string DEFAULT_LOGS_DIRECTORY = "../logs/";
+    inline const std::string DEFAULT_LOGS_DIRECTORY = "../logs/";
 
-    inline static const std::string SIMULATION_MODE_ENV = "SIMULATION_MODE";
+    inline const std::string SIMULATION_MODE_ENV = "SIMULATION_MODE";
 
-    inline static const std::string DEFAULT_SIMULATION_MODE = "FALSE";
+    inline const std::string DEFAULT_SIMULATION_MODE = "FALSE";
 
-    inline static const std::string CONFIG_FILE_PATH_ENV = "CONFIG_FILE_PATH";
+    inline const std::string CONFIG_FILE_PATH_ENV = "CONFIG_FILE_PATH";
     
-    inline static const std::string DEFAULT_CONFIG_FILE_PATH = "../manifest.json";
+    inline const std::string DEFAULT_CONFIG_FILE_PATH = "../manifest.json";
 
-    inline static const std::string TIME_SYNC_TOPIC_ENV = "TIME_SYNC_TOPIC";
+    inline const std::string TIME_SYNC_TOPIC_ENV = "TIME_SYNC_TOPIC";
 
-    inline static const std::string DEFAULT_TIME_SYNC_TOPIC = "time_sync";
+    inline const std::string DEFAULT_TIME_SYNC_TOPIC = "time_sync";
 
     
 }
