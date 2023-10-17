fce23a519ca1f520cae00e7c44ca2895ad6bbf2a
dev <paul.k.bourelly@leidos.com>
2023-10-17 16:02:52 -0400

    Update Unit tests and add license headers

--- streets_utils/streets_messages/test/heading_confidence_test.cpp
+++ streets_utils/streets_messages/test/heading_confidence_test.cpp
@@ -1,3 +1,16 @@
+// Copyright 2019-2023 Leidos
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 #include <gtest/gtest.h>
 #include <sensor_data_sharing_msg/heading_confidence.hpp>
 
@@ -12,8 +25,6 @@ TEST(heading_confidence_test, test_from_int){
     EXPECT_EQ(heading_confidence::PREC_0_05_deg, heading_confidence_from_int(5));
     EXPECT_EQ(heading_confidence::PREC_0_01_deg, heading_confidence_from_int(6));
     EXPECT_EQ(heading_confidence::PREC_0_0125_deg, heading_confidence_from_int(7));
-
-
     // Value outside of range set to unavailable
     EXPECT_EQ(heading_confidence::UNAVAILABLE, heading_confidence_from_int(8));
 
