2209c4459d718e7815a477f1c58c42c68e81fa2e
dev <paul.k.bourelly@leidos.com>
2023-11-29 14:01:49 -0500

    PR Updates

--- sensor_data_sharing_service/src/sensor_data_sharing_service.cpp
+++ sensor_data_sharing_service/src/sensor_data_sharing_service.cpp
@@ -1,6 +1,7 @@
 #include "sensor_data_sharing_service.hpp"
 
 namespace sensor_data_sharing_service {
+    
     using namespace streets_service;
     using namespace streets_utils::messages;
     sds_service::~sds_service() {
@@ -41,7 +42,7 @@ namespace sensor_data_sharing_service {
                 }
             }
         }
-        catch (const std::runtime_error &e) {
+        catch (const streets_utils::json_utils::json_parse_exception &e) {
             SPDLOG_ERROR("Exception occured consuming detection message : {0}", e.what());
         }
 
@@ -51,46 +52,13 @@ namespace sensor_data_sharing_service {
         try{
             SPDLOG_INFO("Starting SDSM Producer!");
             while ( sdsm_producer && sdsm_producer->is_running() ) {
-                // TODO Remove after testing
-                streets_utils::messages::sdsm::sensor_data_sharing_msg msg;
-                msg._equipment_type = streets_utils::messages::sdsm::equipment_type::RSU;
-                msg._msg_count = 255;
-                msg._source_id = "00000001";
-                msg._ref_positon._latitude=900000001;
-                msg._ref_positon._longitude=1800000001;
-                msg._time_stamp.second = 65535;
-                msg._time_stamp.minute = 60;
-                msg._time_stamp.hour= 31;
-                msg._time_stamp.day = 31;
-                msg._time_stamp.month = 12;
-                msg._time_stamp.year = 4095; // Max
-                msg._ref_position_confidence._semi_major_axis_accuracy = 255;
-                msg._ref_position_confidence._semi_minor_axis_accuracy = 255;
-                msg._ref_position_confidence._semi_major_axis_orientation = 65535;
-                // Add Detected Object
-                streets_utils::messages::sdsm::detected_object_data detected_object;
-                detected_object._detected_object_common_data._object_id = 65525;
-                detected_object._detected_object_common_data._object_type = streets_utils::messages::sdsm::object_type::ANIMAL;
-                detected_object._detected_object_common_data._classification_confidence = 101;
-                detected_object._detected_object_common_data._heading = 28800;
-                detected_object._detected_object_common_data._heading_confidence = streets_utils::messages::sdsm::heading_confidence::PREC_01_deg;
-                detected_object._detected_object_common_data._position_offset._offset_x = 32767;
-                detected_object._detected_object_common_data._position_offset._offset_y = 32767;
-                detected_object._detected_object_common_data._position_offset._offset_z = 32767;
-                detected_object._detected_object_common_data._pos_confidence._position_confidence = streets_utils::messages::sdsm::position_confidence::A_10CM;
-                detected_object._detected_object_common_data._pos_confidence._elevation_confidence =  streets_utils::messages::sdsm::position_confidence::A_1M;
-                detected_object._detected_object_common_data._speed = 8191;
-                detected_object._detected_object_common_data._time_measurement_offset = 1500;
-                detected_object._detected_object_common_data._time_confidence = streets_utils::messages::sdsm::time_confidence::TIME_000_000_002;
-                msg._objects.push_back(detected_object);
-                const std::string json_msg = streets_utils::messages::sdsm::to_json(msg);
-                SPDLOG_DEBUG("Sending SDSM : {0}", json_msg);
-                // TODO Remove after testing
-                sdsm_producer->send(json_msg);           
+                // const std::string json_msg = streets_utils::messages::sdsm::to_json(msg);
+                // SPDLOG_DEBUG("Sending SDSM : {0}", json_msg);
+                // sdsm_producer->send(json_msg);           
                 streets_clock_singleton::sleep_for(1000); // Sleep for 10 second between publish  
             }
         }
-        catch( const std::runtime_error &e) {
+        catch( const streets_utils::json_utils::json_parse_exception &e) {
             SPDLOG_ERROR("Exception occurred producing SDSM : {0}", e.what());
         }
     }
